382722e5cc2853e5e6f39e110761b4921d7bb1e5
배홍섭 <baehongseob@baehongseob-ui-MacBookPro.local>
2022-06-16 15:26:10 +0900

    Updates

--- jjjj.js
+++ jjjj.js
@@ -1,5 +1,6 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-  
+// import * as THREE from './node_modules/three/build/three.module.js';
+
+import * as THREE from "./node_modules/three/build/three.module.js";
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { VOXLoader, VOXMesh } from './node_modules/three/examples/jsm/loaders/VOXLoader.js';
 
